6238bf4ffe4439d615ea77e08a99225bcaea5143
jiyeong <jyfullsun42@gmail.com>
2023-11-22 21:29:41 +0900

    Initial commit

---
+++ app/src/main/java/kr/ac/duksung/myapplication/MainActivity.java
@@ -0,0 +1,16 @@
+package kr.ac.duksung.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+import com.prolificinteractive.materialcalendarview.CalendarDay;
+import com.prolificinteractive.materialcalendarview.CalendarMode;
+import com.prolificinteractive.materialcalendarview.MaterialCalendarView;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/kr/ac/duksung/myapplication/cartActivity.java
@@ -0,0 +1,4 @@
+package kr.ac.duksung.myapplication;
+
+public class cartActivity {
+}
